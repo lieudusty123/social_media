76de0508be059489a7a0f8669cee3a9eccc9d751
Omer Ezra <90283262+lieudusty123@users.noreply.github.com>
2023-04-08 23:01:00 +0300

    changed server port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 // const buildPath = path.join(__dirname, "build");
 // console.log(buildPath);
 //file setup
